763f11796510dee3ed5d99c925a54fe1eadfa9b7
Daniel Fisher <danielg16fisher@gmail.com>
2025-07-22 11:47:41 +0300

    Trainer Complete

--- Models/Predictor.py
+++ Models/Predictor.py
@@ -1,28 +1,18 @@
 import numpy as np
 
-class NaiveBayesTrainer:
+class NaiveBayesPredictor:
     def __init__(self, store):
         self.store = store
 
-    def fit(self, X, y):
-        self.store.set_classes(y)
-        total_samples = len(y)
-
+    def predict_single(self, x):
+        posteriors = {}
         for c in self.store.classes:
-            X_c = X[y == c]
-            self.store.store_prior(c, len(X_c) / total_samples)
+            log_prob = np.log(self.store.class_priors[c])
+            for i, val in enumerate(x):
+                prob = self.store.get_likelihood(i, val, c)
+                log_prob += np.log(prob)
+            posteriors[c] = log_prob
+        return max(posteriors, key=posteriors.get)
 
-            for i in range(X.shape[1]):
-                values, counts = np.unique(X_c[:, i], return_counts=True)
-
-                if i not in self.store.feature_values:
-                    self.store.feature_values[i] = np.unique(X[:, i])
-
-                total_count = sum(counts)
-                k = len(self.store.feature_values[i])
-                value_count_dict = dict(zip(values, counts))
-
-                for val in self.store.feature_values[i]:
-                    count = value_count_dict.get(val, 0)
-                    prob = (count + 1) / (total_count + k)
-                    self.store.store_likelihood(i, val, c, prob)
+    def predict(self, X):
+        return [self.predict_single(x) for x in X]
--- Models/Trainer.py
+++ Models/Trainer.py
@@ -5,5 +5,24 @@ class NaiveBayesTrainer:
         self.store = store
 
     def fit(self, X, y):
-        pass
+        self.store.set_classes(y)
+        total_samples = len(y)
 
+        for c in self.store.classes:
+            X_c = X[y == c]
+            self.store.store_prior(c, len(X_c) / total_samples)
+
+            for i in range(X.shape[1]):
+                values, counts = np.unique(X_c[:, i], return_counts=True)
+
+                if i not in self.store.feature_values:
+                    self.store.feature_values[i] = np.unique(X[:, i])
+
+                total_count = sum(counts)
+                k = len(self.store.feature_values[i])
+                value_count_dict = dict(zip(values, counts))
+
+                for val in self.store.feature_values[i]:
+                    count = value_count_dict.get(val, 0)
+                    prob = (count + 1) / (total_count + k)
+                    self.store.store_likelihood(i, val, c, prob)
